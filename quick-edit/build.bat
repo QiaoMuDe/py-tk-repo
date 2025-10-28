a4047a011d1fd17af84395f4c6b2823572d2d0d5
M乔木 <2776617348@qq.com>
2025-10-28 09:55:34 +0800

    build(quick-edit): 更新构建脚本中的入口文件名
    将 build.bat 脚本中的源文件名从 QuickEdit.py 更改为 main.py，
    以匹配项目当前的文件结构。

--- quick-edit/build.bat
+++ quick-edit/build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter  QuickEdit.py
+nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter  main.py
