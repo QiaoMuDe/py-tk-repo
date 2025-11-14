8cb24f3bf03fc5a430d2feed453f66527fdb817e
M乔木 <2776617348@qq.com>
2025-11-14 15:08:34 +0800

    build(quick-edit++): 更新构建脚本中的目录重命名命令
    将构建脚本中的 "cd QuickEdit++.dist" 命令更改为 "move QuickEdit++.dist QuickEdit++",
    以正确处理构建后的目录重命名操作。

--- quick-edit++/build.bat
+++ quick-edit++/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 @REM nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter --windows-icon-from-ico=.\ico\QuickEdit++.ico  .\QuickEdit++.py
-@REM cd QuickEdit++.dist
+@REM move QuickEdit++.dist QuickEdit++
 @REM 7z a -tzip nuitka-QuickEditPlus.zip QuickEdit++
 
 pyinstaller.exe -w -D -i .\ico\QuickEdit++.ico .\QuickEdit++.py
