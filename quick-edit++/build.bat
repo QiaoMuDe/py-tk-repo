484506d734a9dfcdc44a001b4e5841d1a7e005e1
M乔木 <2776617348@qq.com>
2025-11-14 11:57:41 +0800

    build(quick-edit++): 更新构建脚本中的Nuitka和PyInstaller配置
    修改构建脚本以使用新的图标文件路径和主程序文件名,
    将Nuitka命令中的QuickEdit.ico更新为QuickEdit++.ico,
    并将PyInstaller命令中的文件路径统一使用相对路径格式。

--- quick-edit++/build.bat
+++ quick-edit++/build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-@REM nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter --windows-icon-from-ico=icos\QuickEdit.ico  QuickEdit.py
+@REM nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter --windows-icon-from-ico=.\ico\QuickEdit++.ico  .\QuickEdit++.py
 
 pyinstaller.exe -w -D -i .\ico\QuickEdit++.ico .\QuickEdit++.py
