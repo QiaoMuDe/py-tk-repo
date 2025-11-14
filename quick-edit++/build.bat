b4be6a7aeaccd896a1fb70d71f960d9f7efb567e
M乔木 <2776617348@qq.com>
2025-11-14 15:01:23 +0800

    build(quick-edit++): 更新构建脚本以支持PyInstaller打包和ZIP压缩
    修改构建脚本, 添加PyInstaller打包后的目录切换和7-Zip压缩命令,
    用于创建可分发的ZIP

--- quick-edit++/build.bat
+++ quick-edit++/build.bat
@@ -1,5 +1,9 @@
 @echo off
 
 @REM nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter --windows-icon-from-ico=.\ico\QuickEdit++.ico  .\QuickEdit++.py
+@REM cd QuickEdit++.dist
+@REM 7z a -tzip nuitka-QuickEditPlus.zip QuickEdit++
 
 pyinstaller.exe -w -D -i .\ico\QuickEdit++.ico .\QuickEdit++.py
+cd dist
+7z a -tzip pyinstaller-QuickEditPlus.zip QuickEdit++
