b39a1279240e4b498cf5affbc735591ccc4d3937
M乔木 <2776617348@qq.com>
2025-11-16 19:06:52 +0800

    fix(build): 修正构建脚本中的图标路径错误
    将构建脚本中使用的图标路径从 "icos" 更正为 "icons"，
    确保打包工具能够正确找到并使用应用程序图标。

--- quick-edit++/build.bat
+++ quick-edit++/build.bat
@@ -1,9 +1,9 @@
 @echo off
 
-@REM nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter --windows-icon-from-ico=.\icos\QuickEdit++.ico  .\QuickEdit++.py
+@REM nuitka --standalone --windows-console-mode=disable --enable-plugin=tk-inter --windows-icon-from-ico=.\icons\QuickEdit++.ico  .\QuickEdit++.py
 @REM move QuickEdit++.dist QuickEdit++
 @REM 7z a -tzip nuitka-QuickEditPlus.zip QuickEdit++
 
-pyinstaller.exe -w -D -i .\icos\QuickEdit++.ico .\QuickEdit++.py
+pyinstaller.exe -w -D -i .\icons\QuickEdit++.ico .\QuickEdit++.py
 cd dist
 7z a -tzip pyinstaller-QuickEditPlus.zip QuickEdit++
