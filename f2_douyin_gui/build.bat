5b01882eb8f287e64cf4cc64065957c95f653528
M乔木 <2776617348@qq.com>
2025-12-03 19:55:05 +0800

    build(f2_douyin_gui): 移除PyInstaller打包命令中的冗余参数

--- f2_douyin_gui/build.bat
+++ f2_douyin_gui/build.bat
@@ -2,4 +2,4 @@
 
 @REM nuitka --standalone --show-scons --windows-console-mode=disable --enable-plugin=tk-inter  f2_douyin_gui.py
 
-pyinstaller.exe -w -D -i f2_douyin_gui_ctk.py
+pyinstaller.exe -w -D f2_douyin_gui_ctk.py
